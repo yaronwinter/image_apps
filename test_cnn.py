999622e923a8f4e5bec9044fbe865ec09ec00332
Yaron Winter <yaron.winter@gmail.com>
2024-07-09 13:01:34 +0300

    dl hw 4

--- test_cnn.py
+++ test_cnn.py
@@ -12,7 +12,7 @@ def test(model: nn.Module, test_set: DataLoader, device: torch.device) -> float:
         with torch.no_grad():
             logits = model(images.to(device))
         preds = torch.argmax(logits, dim=1)
-        corrects += (preds == labels).sum().item()
+        corrects += (preds == labels.to(device)).sum().item()
         evaluated += images.size()[0]
         
     return (corrects / evaluated)
